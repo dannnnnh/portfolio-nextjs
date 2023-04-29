1eed9813670bf1fa854e9cfba660d99bc0d90212
dannnnnh <113985769+dannnnnh@users.noreply.github.com>
2023-04-29 22:26:20 +0200

    Updating for vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,8 @@
 import "../styles/globals.css";
-
-
-
-
-
-
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-
   return <Component {...pageProps} />;
 
+  <Analytics />;
 }
